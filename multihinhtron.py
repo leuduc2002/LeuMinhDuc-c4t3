7e4402f0ca9db0d265a912d9ba79995406e216c3
leuduc2002 <leuminhduc2002@gmail.com>
2018-06-06 23:46:10 +0700

    first commit

---
+++ multihinhtron.py
@@ -0,0 +1,9 @@
+from turtle import *
+shape("turtle")
+speed(-1)
+
+for i in range(50):
+    circle(50)
+    left(10)
+
+mainloop()
---
+++ session2.py
@@ -0,0 +1,11 @@
+ ptb2(a,b,c)
+    print a,b,c
+    import math
+    delta = b*b-4*a*c
+    if (delta < 0):
+        print "phuong trinh deo co nghiem"
+    if (delta = 0)
+        x1 = (-b)/(2*a)
+        print "phuong trinh co nghiem kep"
+
+
